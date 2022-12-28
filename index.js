435c2aed05a83d5ce7c3b50b9896e73adf1215ba
fransua228 <lapapapa605@gmail.com>
2022-12-29 03:33:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const { exec } = require('child_process')
+const {join} = require('path')
+const WebSocket = require('ws')
+
+const port = process.env.PORT;
+
+const commands = [
+    'xz',
+    'my',
+]
+
+const execFunction = (err, stdout, stderr) => {
+    if (err) return console.error(err);
+    console.log(stdout);
+}
+
+const ws = new WebSocket(process.env.SERVER_URL);
+
+ws.on('open', function open() {
+  ws.send('Connected and work');
+});
+
+ws.on('message', function message(data) {
+    if(process.env.COMMANDS.includes(String(data))) exec(join(__dirname,"bats",data + ".bat"), execFunction);
+  console.log('received: %s', data);
+});
+
+app.listen(port, () => console.log(`Work ${port}`))
